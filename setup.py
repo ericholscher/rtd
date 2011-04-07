85336baab241b025c4e2f16898e3e2a265e2837e
Eric Holscher <eric@ericholscher.com>
2011-04-07 12:54:09 -0700

    Because I can.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='http://github.com/ericholscher/rtd',
-    scripts=['bin/rtd'],
+    scripts=['bin/rtd', 'bin/rtfd'],
     license='BSD',
     description='Find documentation on Read the Docs!',
     long_description=codecs.open("README.rst", "r", "utf-8").read()
