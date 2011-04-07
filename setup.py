d1be51f8dc0be049afe3614e7889a4646727a5c6
Eric Holscher <eric@ericholscher.com>
2011-04-06 22:51:35 -0700

    Fuck python packaging.

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-import commands
 from distutils.core import setup
 
 setup(
     name='docs.py',
-    version='1.0',
+    version='1.0.1',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='http://github.com/ericholscher/docs.py',
     scripts=['bin/docs.py'],
     license='BSD',
     description='Find documentation on Read the Docs!',
-    requires=['httplib2 (==0.6.0)'],
 )
 
