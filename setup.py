4eacb6258ce833c678fe190bcc6d0986d0ba6c25
Eric Holscher <eric@ericholscher.com>
2011-04-12 19:23:27 -0700

    Handle setuptools/distutils and requirements for setuptools.
    Thanks to jek for the pointer.

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from distutils.core import setup
 import codecs
+try:
+    from setuptools import setup
+    extra_setup = dict(
+        zip_safe = True,
+        install_requires = ['httplib2'],
+        )
+except ImportError:
+    from distutils.core import setup
+    extra_setup = {}
 
 setup(
     name='rtd',
-    version='1.0.2',
+    version='1.0.3',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='http://github.com/ericholscher/rtd',
-    scripts=['bin/rtd', 'bin/rtfd'],
+    scripts=['bin/rtd'],
     license='BSD',
     description='Find documentation on Read the Docs!',
-    long_description=codecs.open("README.rst", "r", "utf-8").read()
+    long_description=codecs.open("README.rst", "r", "utf-8").read(),
+    **extra_setup
 )
 
